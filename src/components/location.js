6e5c5ab7193c6e37f9e692c904e63a82b2087742
sean1130 <39747545+sean1130@users.noreply.github.com>
2018-07-05 16:08:42 -0600

    updated locations

--- src/components/location.js
+++ src/components/location.js
@@ -4,8 +4,6 @@ export default class Location extends Component {
   render() {
     return (
 
-
-
     <div class="location__container">  
 
       <div class="location__title">
@@ -16,18 +14,18 @@ export default class Location extends Component {
 
             <div class="location__af">
                 <h2>Bottega Movies 9</h2>
-                <h3>American Fork</h3>
+                <h4>American Fork</h4>
             </div>
 
             <div class="location__draper">
                 <h2>Bottega Movies XD</h2>
-                <h3>Draper</h3>
+                <h4>Draper</h4>
             </div>
 
 
             <div class="location__layton">
                 <h2>Bottega Landing XD</h2>
-                <h3>Layton</h3>
+                <h4>Layton</h4>
             </div>
 
         </div>
@@ -36,18 +34,18 @@ export default class Location extends Component {
 
             <div class="locationsTwo__or">
                 <h2>Bottega Movies 16</h2>
-                <h3>Orem</h3>
+                <h4>Orem</h4>
             </div>
 
             <div class="locationsTwo__sj">
                 <h2>Bottega Movies 21</h2>
-                <h3>South Jordan</h3>
+                <h4>South Jordan</h4>
             </div>
 
 
             <div class="locationsTwo__lehi">
                 <h2>Bottega Landing 11</h2>
-                <h3>Lehi</h3>
+                <h4>Lehi</h4>
             </div>
 
         </div>
@@ -73,6 +71,34 @@ export default class Location extends Component {
                     <h6>Browse the Frequently Asked Questions to learn more about Movie Club</h6>
                 </div>
             </div>
+
+
+             <div class="locationabout__title">
+                <h1>About Bottega</h1>
+             </div>
+
+            <div class="locationabout">
+                <div class="locationabout__au">
+                   <img src="http://www.patrickbetdavid.com/wp-content/uploads/2016/10/movies-red.jpg" title=""></img>
+                    <h4>About Us</h4>
+                    <h6>Learn more about our Theatres</h6>
+                </div>
+
+                <div class="locationabout__cu">
+                <img src="http://www.patrickbetdavid.com/wp-content/uploads/2016/10/movies-red.jpg" title=""></img>
+                    <h4>Contact Us</h4>
+                    <h6>We're here to help. contact us for guest services or more information</h6>
+                </div>
+
+
+                <div class="locationabout__ad">
+                <img src="https://thehaletelescope.com/wp-content/uploads/2017/08/war-movies.jpg" title=""></img>
+                    <h4>Advertise with Us</h4>
+                    <h6>We offer several advertising avenues for reaching moviegoers</h6>
+                </div>
+            </div>
+
+
     </div>
 
     );
