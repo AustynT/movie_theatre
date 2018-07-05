4431cc4d22cf0ea8b7930ee63d015143e688edad
sean1130 <39747545+sean1130@users.noreply.github.com>
2018-07-05 15:13:19 -0600

    updated locations

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Location from './location';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>DevCamp React Starter</h1>
+        
+        <Location/>
+      
       </div>
     );
   }
 }
+          
---
+++ src/components/location.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+
+export default class Location extends Component {
+  render() {
+    return (
+
+
+
+    <div class="location__container">  
+
+      <div class="location__title">
+        <h1>Movie Locations</h1>
+      </div>  
+
+        <div class ="location">
+
+            <div class="location__af">
+                <h2>Bottega Movies 9</h2>
+                <h3>American Fork</h3>
+            </div>
+
+            <div class="location__draper">
+                <h2>Bottega Movies XD</h2>
+                <h3>Draper</h3>
+            </div>
+
+
+            <div class="location__layton">
+                <h2>Bottega Landing XD</h2>
+                <h3>Layton</h3>
+            </div>
+
+        </div>
+
+        <div class ="locationsTwo">
+
+            <div class="locationsTwo__or">
+                <h2>Bottega Movies 16</h2>
+                <h3>Orem</h3>
+            </div>
+
+            <div class="locationsTwo__sj">
+                <h2>Bottega Movies 21</h2>
+                <h3>South Jordan</h3>
+            </div>
+
+
+            <div class="locationsTwo__lehi">
+                <h2>Bottega Landing 11</h2>
+                <h3>Lehi</h3>
+            </div>
+
+        </div>
+
+       <div class="locationfaq">
+        <h1>FAQ</h1>
+        </div>
+
+            <div class="locationfaq__vr">
+                <h4>VIP Room FAQ</h4>
+                <h5>Information about The VIP room, making reservations</h5>
+            </div>
+
+            <div class="locationfaq__giftcard">
+                <h4>Gift Cards FAQ</h4>
+                <h5>Information on buying, registering, reloading and purchasing</h5>
+            </div>
+
+            <div class="locationfaq__movieclub">
+                <h4>Movie Club FAQ</h4>
+                <h5>Browse the Frequently Asked Questions to learn more about Movie Club</h5>
+            </div>
+        
+      
+
+      </div>
+    );
+  }
+}
